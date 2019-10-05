9c7ea7a51f0e1e7560886db06277f54a8738765d
TerryJIAN <kiss0817nice@gmail.com>
2019-10-05 17:57:52 +0800

    PYTHON

--- 7688mqttOK.py
+++ 7688mqttOK.py
@@ -6,20 +6,19 @@ global s
 s=serial.Serial("/dev/ttyS0", 57600,timeout = 0.2)
 
 def on_connect(client, userdata, flags, rc):
-         client.publish("LinkIT", json.dumps({"username": user, "message": "Hello, anyone!"}))
-         client.subscribe("LinkIT")
+         client.publish("Data", json.dumps({"username": user, "message": "Hello, anyone!"}))
+         client.subscribe("Ctrl")
 def on_message(client, userdata, msg):
          if  str(msg.payload):
 #               s.write(str(msg.payload)+'\n')
-#               print str(msg.payload)
-	            s.write(str(payload.get("say"))+'\n')
-
+                payload = json.loads(msg.payload.decode())
+                s.write(str(payload.get("message"))+'\n')
 if __name__=='__main__':
          client = mqtt.Client()
          client.on_connect = on_connect
          client.on_message = on_message
 
-         HOST="192.168.2.111"
+         HOST="192.168.1.14"
          client.connect(HOST, 1883, 60)
 
          user = "DUO"
@@ -28,4 +27,4 @@ if __name__=='__main__':
          while True:
                  str1= str(s.readline())
                  if (str1):
-                         client.publish("LinkIT", json.dumps({"username":"arduino", "message": str1}))
+                         client.publish("Data", json.dumps({"username":"arduino", "message": str1}))
